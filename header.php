ab9507ca2fe2076b02762c6a87e5cba13a788c24
HUGE | Raed Atoui <ratoui@hugeinc.com>
2017-03-21 11:28:26 -0400

    comment out nav for now

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'prh-wp-theme' ); ?></a>
+<!--	<a class="skip-link screen-reader-text" href="#content">--><?php //esc_html_e( 'Skip to content', 'prh-wp-theme' ); ?><!--</a>-->
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
@@ -39,8 +39,8 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'prh-wp-theme' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
+<!--			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">--><?php //esc_html_e( 'Primary Menu', 'prh-wp-theme' ); ?><!--</button>-->
+<!--			--><?php //wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
