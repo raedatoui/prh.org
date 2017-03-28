6f1143eae955a50d799ff214fcdbc462cc143d84
HUGE | Raed Atoui <ratoui@hugeinc.com>
2017-03-28 13:21:34 -0400

    remove stuff from footer

--- footer.php
+++ footer.php
@@ -14,11 +14,7 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'prh-wp-theme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'prh-wp-theme' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'prh-wp-theme' ), 'prh-wp-theme', '<a href="https://hugeinc.com/" rel="designer">hugeinc.com</a>' ); ?>
-		</div><!-- .site-info -->
+
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
