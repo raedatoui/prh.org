25cad0c973b9df73523fa6079f1a8317c171f791
HUGE | Raed Atoui <ratoui@hugeinc.com>
2017-04-06 01:40:34 -0400

    stats template in php

--- template-parts/modules/statistics.php
+++ template-parts/modules/statistics.php
@@ -1,5 +1,5 @@
 <?php $stats = $this->modules[STATISTICS_MODULE['name']]; ?>
-<section class="our-focus module page-content">
+<section class="stats module page-content">
 	<?php if ( $stats['config'][MODULE_OPTIONS['title']] != '' ): ?>
 		<div class="module__title">
 			<h2><?php echo $stats['config'][MODULE_OPTIONS['title']] ?></h2>
@@ -7,21 +7,19 @@
 	<?php endif; ?>
 	<div class="module__content">
 		<div class="row">
-			<div class="stats">
-				<?php foreach ( $stats[STATISTICS_MODULE['figures']] as $index => $figure ): ?>
-					<? $figure = $figure[STATISTICS_MODULE['figure']][0]; ?>
-					<div class="stat stat-<?php echo $index ?>">
-						<span><?php echo $figure[STATISTICS_MODULE['number']] ?></span>
-						<span><?php echo $figure[STATISTICS_MODULE['text']] ?></span>
-					</div>
-				<?php endforeach; ?>
-			</div>
+			<?php foreach ( $stats[STATISTICS_MODULE['figures']] as $index => $figure ): ?>
+				<? $figure = $figure[STATISTICS_MODULE['figure']][0]; ?>
+				<div class="stat-title col-xs-12 col-md-4">
+					<span class="stat-number"><?php echo $figure[STATISTICS_MODULE['number']] ?></span>
+					<span class="stat-category"><?php echo $figure[STATISTICS_MODULE['text']] ?></span>
+				</div>
+			<?php endforeach; ?>
 		</div>
-	</div>
-	<div class="row">
-		<?php $cta = $stats['config'][MODULE_OPTIONS['cta']][0];  ?>
-		<a class="cta" href="<? echo $cta[CTA_COMPONENT['link']];?>">
-			<? echo $cta[CTA_COMPONENT['label']]; ?>
-		</a>
+		<div class="row">
+			<?php $cta = $stats['config'][MODULE_OPTIONS['cta']][0];  ?>
+			<a class="cta" href="<? echo $cta[CTA_COMPONENT['link']];?>">
+				<? echo $cta[CTA_COMPONENT['label']]; ?>
+			</a>
+		</div>
 	</div>
 </section>
