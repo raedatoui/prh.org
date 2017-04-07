218e14af15c18f578884f2d8df70d14a9d11420d
HUGE | Raed Atoui <ratoui@hugeinc.com>
2017-04-07 11:22:44 -0400

    check for non required text field

--- template-parts/modules/quote.php
+++ template-parts/modules/quote.php
@@ -1,4 +1,11 @@
-<?php $quote = $this->modules[QUOTE_MODULE['name']]; ?>
+<?php
+	$quote = $this->modules[QUOTE_MODULE['name']];
+	$quoteText = $quote[QUOTE_MODULE['text']];
+	$quoteClass = 'col-md-8';
+	if ( $quoteText == '') {
+		$quoteClass = 'col-md-12';
+	}
+?>
 <section class="module module__quote page-content">
 	<?php if ( $quote['config'][MODULE_OPTIONS['title']] != '' ): ?>
 		<div class="module__title">
@@ -7,10 +14,12 @@
 	<?php endif; ?>
 	<div class="module__content">
 		<div class="row">
-			<div class="text-component col-xs-12 col-md-4">
-				<p><?php echo $quote[QUOTE_MODULE['text']]; ?></p>
-			</div>
-			<div class="quote-component col-xs-12 col-md-8">
+			<?php if ( $quoteText != '') : ?>
+				<div class="text-component col-xs-12 col-md-4">
+					<p><?php echo  $quoteText; ?></p>
+				</div>
+			<?php endif; ?>
+			<div class="quote-component col-xs-12 <?php echo $quoteClass ?>">
 				<blockquote>
 					<p><?php echo $quote[QUOTE_MODULE['quote']]; ?></p>
 					<footer>
