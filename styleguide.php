694bf7e1654eabadc13499be14c1e716b680bf02
Jack Gold <jgold@H9048.ad.hugeinc.com>
2017-04-12 15:47:02 -0400

    clarifying text in the headers & copy area of the styleguide

--- styleguide.php
+++ styleguide.php
@@ -20,23 +20,25 @@ get_header(); ?>
 <section class="module">
   <div class="content">
     <div class="module__title">
-      <h2>Headers and copy</h2>
+      <h2>Copy</h2>
     </div>
     <p class="lead-copy">This paragraph text is larger than normal. It’s generally used at the beginning of a page or section, to introduce the content. Blocks of text in this style shouldn’t be too long.</p>
     <p>This is normal paragraph text. These styles apply to most of the body copy throughout the site. They support styles like <strong>bold</strong> and <em>italic</em> and can contain <a href="#">links</a> to other pages.</p>
 
-    <p>The level-one heading generally just happens once on a page, up in the hero or header area. It’s usually the page’s title. On this page, it's "Style Guide".</p>
+    <p>The level-one heading generally just happens once on a page, up in the hero or header area. It’s usually the page’s title. On this page, it's "Style Guide". And here's some filler copy from the PRH site, to show what a larger block of text looks like.</p>
+
+    <p>Physicians for Reproductive Health believes that access to contraception is of vital importance to women’s health because it allows them to determine the timing and spacing of pregnancies. In the medical world, we have studied birth control methods and their effects for decades. We know, based on enormous amounts of scientific evidence, that contraception is crucial to women’s well-being, their children’s health, and their ability to educate themselves, achieve career goals, care for and support their families, and otherwise participate in society. As physicians, we see every day how it improves our patients’ lives.</p>
   </div>
 </section>
 <section class="module">
   <div class="content">
     <div class="module__title">
-      <h2>Heading level two (h2)</h2>
+      <h2>Headers</h2>
     </div>
-    <p>The second-level heading, or module title, is used for naming the big subsections within a page.</p>
+    <p class="lead-copy">Headers show content hierarchy.</p>
+    <p>The second-level heading, or module title, is used for naming the big subsections within a page. The word 'Headers' above this block of text is an example.</p>
     <h3>Heading level three (h3)</h3>
-    <p>Following the page’s information hierarchy, third level headings should be used within the subsections. Avoid skipping levels – if your last header was an h2 and you want to label a block within that subsection, use an h3. If you need one within <em>that</em>, go ahead to h4. Here are a few more paragraphs of text from the PRH site, to show what a larger block of text looks like.</p>
-    <p>Physicians for Reproductive Health believes that access to contraception is of vital importance to women’s health because it allows them to determine the timing and spacing of pregnancies. In the medical world, we have studied birth control methods and their effects for decades. We know, based on enormous amounts of scientific evidence, that contraception is crucial to women’s well-being, their children’s health, and their ability to educate themselves, achieve career goals, care for and support their families, and otherwise participate in society. As physicians, we see every day how it improves our patients’ lives.</p>
+    <p>Following the page’s information hierarchy, third level headings should be used within the subsections. Avoid skipping levels – if your last header was an h2 and you want to label a block within that subsection, use an h3. If you need one within <em>that</em>, continue on with h4. HTML supports 6 levels of headers, but most content won't go deeper than 3 or 4.</p>
     <h4>Heading level four</h4>
     <p>Some pages probably won’t get to this level of hierarchy, but others might, especially in areas like sidebars. Here's some more filler text.</p>
     <p>Physicians for Reproductive Health believes that access to contraception is of vital importance to women’s health because it allows them to determine the timing and spacing of pregnancies. In the medical world, we have studied birth control methods and their effects for decades.</p>
