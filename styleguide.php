809695f889796d83af8b720a2bbc1db71fcbd422
Jack Gold <jgold@H9048.ad.hugeinc.com>
2017-04-12 15:27:05 -0400

    more global styles where possible, more elements on the styleguide page

--- styleguide.php
+++ styleguide.php
@@ -35,37 +35,58 @@ get_header(); ?>
 
   <section class="module">
     <div class="content">
-      <div class="module-title">
+      <div class="module__title">
         <h2>Headers and copy</h2>
       </div>
-      <h1>Heading level one (h1)</h1>
       <p class="lead-copy">This paragraph text is larger than normal. It’s generally used at the beginning of a page or section, to introduce the content. Blocks of text in this style shouldn’t be too long.</p>
-      <p>This is normal paragraph text. These styles apply to most of the body copy throughout the site. They support styles like <strong>bold</strong> and <em>italic</em> and can contain <a href="#">links</a> to other pages. By the way, the level-one heading generally just happens once on a page, up in the hero or header area. It’s usually the page’s title.</p>
-      <div class="module-title">
+      <p>This is normal paragraph text. These styles apply to most of the body copy throughout the site. They support styles like <strong>bold</strong> and <em>italic</em> and can contain <a href="#">links</a> to other pages.</p>
+
+      <p>The level-one heading generally just happens once on a page, up in the hero or header area. It’s usually the page’s title. On this page, it's "Style Guide".</p>
+    </div>
+  </section>
+  <section class="module">
+    <div class="content">
+      <div class="module__title">
         <h2>Heading level two (h2)</h2>
       </div>
       <p>The second-level heading, or module title, is used for naming the big subsections within a page.</p>
       <h3>Heading level three (h3)</h3>
       <p>Following the page’s information hierarchy, third level headings should be used within the subsections. Avoid skipping levels – if your last header was an h2 and you want to label a block within that subsection, use an h3. If you need one within <em>that</em>, go ahead to h4. Here are a few more paragraphs of text from the PRH site, to show what a larger block of text looks like.</p>
       <p>Physicians for Reproductive Health believes that access to contraception is of vital importance to women’s health because it allows them to determine the timing and spacing of pregnancies. In the medical world, we have studied birth control methods and their effects for decades. We know, based on enormous amounts of scientific evidence, that contraception is crucial to women’s well-being, their children’s health, and their ability to educate themselves, achieve career goals, care for and support their families, and otherwise participate in society. As physicians, we see every day how it improves our patients’ lives.</p>
-      <p>The contraceptives that are most reliable, however, can be too expensive for many women to afford out of pocket. Because we believe every woman should be able to choose from a broad array FDA-approved contraceptives to find the one that meets her health needs, we advocated in support of the Affordable Care Act. We were thrilled when the Supreme Court upheld health care reform in June 2012.</p>
       <h4>Heading level four</h4>
-      <p>Some pages probably won’t ever get to this level of hierarchy, but it’s probably a good idea to provide a style just in case. More filler text:</p>
-      <p>Physicians for Reproductive Health believes that access to contraception is of vital importance to women’s health because it allows them to determine the timing and spacing of pregnancies. In the medical world, we have studied birth control methods and their effects for decades. We know, based on enormous amounts of scientific evidence, that contraception is crucial to women’s well-being, their children’s health, and their ability to educate themselves, achieve career goals, care for and support their families, and otherwise participate in society. As physicians, we see every day how it improves our patients’ lives.</p>
-
-      <div class="module-title">
-        <h2>Other elements</h2>
+      <p>Some pages probably won’t get to this level of hierarchy, but others might, especially in areas like sidebars. Here's some more filler text.</p>
+      <p>Physicians for Reproductive Health believes that access to contraception is of vital importance to women’s health because it allows them to determine the timing and spacing of pregnancies. In the medical world, we have studied birth control methods and their effects for decades.</p>
+    </div>
+  </section>
+  <section class="module">
+    <div class="content">
+      <div class="module__title">
+        <h2>Buttons & links</h2>
       </div>
-      <p><span class="eyebrow">Eyebrow text | Also used for dates</span></p>
-      <div class="cta-row"><a href="#" class="cta">Call To Action</a>
-      <a href="#" class="cta cta--red">Call to Action</a></div>
+      <div class="cta-row">
+        <a href="#" class="cta">Call To Action</a>
+        <a href="#" class="cta cta--red">Call to Action</a>
+      </div>
+      <p><a href="#">A normal inline link looks like this.</a></p>
+    </div>
+  </section>
+  <section class="module">
+    <div class="content">
+    <div class="module__title">
+        <h2>Blockquotes</h2>
+      </div>
       <blockquote>
         <p>This is what the default styling for a blockquote looks like, including a citation with a secondary detail.</p>
         <footer><cite>—Joel Hodgson, <span class="cite-origin">Minnesota</span></cite></footer>
       </blockquote>
-
-
-      <?php //the_content(); ?>
+    </div>
+  </section>
+  <section class="module">
+    <div class="content">
+      <div class="module__title">
+        <h2>Misc text elements</h2>
+      </div>
+      <p><span class="eyebrow">Eyebrow text | Also used for dates</span></p>
     </div>
   </section>
 
