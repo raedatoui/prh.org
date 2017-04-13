426f13eba6f995587529374b9df3c3d5673fa048
HUGE | Raed Atoui <ratoui@hugeinc.com>
2017-04-13 13:04:07 -0400

    get fields for the current post. linting

--- inc/editor.php
+++ inc/editor.php
@@ -22,28 +22,28 @@ define('EDITOR_HIDE_PAGE_TEMPLATES', json_encode(array('homepage.php')));
  * @return void
  */
 function prh_hide_editor() {
-  global $pagenow;
-  if(!('post.php' == $pagenow)){
-    return;
-  }
+	global $pagenow;
+	if(!('post.php' == $pagenow)){
+		return;
+	}
 
-  // Get the Post ID.
-  $post_id = filter_input(INPUT_GET, 'post') ? filter_input(INPUT_GET, 'post') : filter_input(INPUT_POST, 'post_ID');
-  if(!isset($post_id)) {
-    return;
-  }
+	// Get the Post ID.
+	$post_id = filter_input(INPUT_GET, 'post') ? filter_input(INPUT_GET, 'post') : filter_input(INPUT_POST, 'post_ID');
+	if(!isset($post_id)) {
+		return;
+	}
 
-  // Hide the editor on the page titled 'Homepage'
-  if(in_array(get_the_title($post_id), json_decode(EDITOR_HIDE_PAGE_TITLES))) {
-    remove_post_type_support('page', 'editor');
-  }
+	// Hide the editor on the page titled 'Homepage'
+	if(in_array(get_the_title($post_id), json_decode(EDITOR_HIDE_PAGE_TITLES))) {
+		remove_post_type_support('page', 'editor');
+	}
 
-  // Hide the editor on a page with a specific page template
-  $template_filename = get_post_meta($post_id, '_wp_page_template', true);
+	// Hide the editor on a page with a specific page template
+	$template_filename = get_post_meta($post_id, '_wp_page_template', true);
 
-  if(in_array($template_filename, json_decode(EDITOR_HIDE_PAGE_TEMPLATES))) {
-    remove_post_type_support('page', 'editor');
-  }
+	if(in_array($template_filename, json_decode(EDITOR_HIDE_PAGE_TEMPLATES))) {
+		remove_post_type_support('page', 'editor');
+	}
 }
 add_action('admin_init', 'prh_hide_editor');
 
@@ -52,8 +52,8 @@ add_action('admin_init', 'prh_hide_editor');
  * Callback function to insert 'styleselect' into the $buttons array
  */
 function prh_mce_buttons( $buttons ) {
-  array_unshift($buttons, 'styleselect');
-  return $buttons;
+	array_unshift($buttons, 'styleselect');
+	return $buttons;
 }
 add_filter('mce_buttons', 'prh_mce_buttons');
 
@@ -61,19 +61,19 @@ add_filter('mce_buttons', 'prh_mce_buttons');
  * Callback function to insert custom styles into the styleselect dropdown.
  */
 function prh_mce_before_init_insert_formats( $init_array ) {
-  // Define the style_formats array
-  $style_formats = array(
-    // Each array child is a format with it's own settings
-    array(
-      'title' => 'hero-link',
-      'inline' => 'a',
-      'classes' => 'hero__link underline',
-      'wrapper' => false,
-    ),
-  );
-  // Insert the array, JSON ENCODED, into 'style_formats'
-  $init_array['style_formats'] = json_encode($style_formats);
-  return $init_array;
+	// Define the style_formats array
+	$style_formats = array(
+		// Each array child is a format with it's own settings
+		array(
+			'title' => 'hero-link',
+			'inline' => 'a',
+			'classes' => 'hero__link underline',
+			'wrapper' => false,
+		),
+	);
+	// Insert the array, JSON ENCODED, into 'style_formats'
+	$init_array['style_formats'] = json_encode($style_formats);
+	return $init_array;
 }
 add_filter('tiny_mce_before_init', 'prh_mce_before_init_insert_formats');
 
@@ -82,17 +82,17 @@ add_filter('tiny_mce_before_init', 'prh_mce_before_init_insert_formats');
  * typography styles directly in the editor's visual mode.
  */
 function prh_custom_editor_styles() {
-  add_editor_style('css/main.css');
+	add_editor_style('css/main.css');
 }
 add_action('init', 'prh_custom_editor_styles');
 
 
 function set_post_order_in_admin( $wp_query ) {
-  global $pagenow;
-  if ( is_admin() && 'edit.php' == $pagenow && !isset($_GET['orderby'])) {
-      $wp_query->set( 'orderby', 'date' );
-      $wp_query->set( 'order', 'DESC' );
-  }
+	global $pagenow;
+	if ( is_admin() && 'edit.php' == $pagenow && !isset($_GET['orderby'])) {
+			$wp_query->set( 'orderby', 'date' );
+			$wp_query->set( 'order', 'DESC' );
+	}
 }
 
 add_filter('pre_get_posts', 'set_post_order_in_admin', 5 );
@@ -102,17 +102,14 @@ add_filter('pre_get_posts', 'set_post_order_in_admin', 5 );
  * Add a quick link to the homepage in the admin menu, because convenience.
  **/
 function prh_admin_menu() {
-    $front_id = get_option(page_on_front);
-    $front_slug = 'post.php?post=' . $front_id . '&action=edit';
+		$front_id = get_option(page_on_front);
+		$front_slug = 'post.php?post=' . $front_id . '&action=edit';
 
-    add_pages_page(
-        'Home',
-        'Home',
-        'edit_pages',
-        $front_slug);
+		add_pages_page(
+				'Home',
+				'Home',
+				'edit_pages',
+				$front_slug);
 }
 
 add_action('admin_menu', 'prh_admin_menu');
-
-
-?>
--- styleguide.php
+++ styleguide.php
@@ -8,109 +8,110 @@
 get_header(); ?>
 
 
-
 <section class="hero module">
-  <div class="content">
-    <h1 class="hero__header">Style Guide
-    </h1>
-    <div class="col-xs-12 hero__subhead">An inventory of global patterns.</div>
+	<div class="content">
+		<h1 class="hero__header">Style Guide</h1>
+		<div class="col-xs-12 hero__subhead">An inventory of global patterns.</div>
+	</div>
+</section>
+
+<section class="module">
+	<div class="content">
+		<div class="module__title">
+			<h2>Copy</h2>
+		</div>
+		<p class="lead-copy">This paragraph text is larger than normal. It’s generally used at the beginning of a page or section, to introduce the content. Blocks of text in this style shouldn’t be too long.</p>
+		<p>This is normal paragraph text. These styles apply to most of the body copy throughout the site. They support styles like <strong>bold</strong> and <em>italic</em> and can contain <a href="#">links</a> to other pages.</p>
+		<p>The level-one heading generally just happens once on a page, up in the hero or header area. It’s usually the page’s title. On this page, it's "Style Guide". And here's some filler copy from the PRH site, to show what a larger block of text looks like.</p>
+		<p>Physicians for Reproductive Health believes that access to contraception is of vital importance to women’s health because it allows them to determine the timing and spacing of pregnancies. In the medical world, we have studied birth control methods and their effects for decades. We know, based on enormous amounts of scientific evidence, that contraception is crucial to women’s well-being, their children’s health, and their ability to educate themselves, achieve career goals, care for and support their families, and otherwise participate in society. As physicians, we see every day how it improves our patients’ lives.</p>
+	</div>
+</section>
+
+<section class="module">
+	<div class="content">
+		<div class="module__title">
+			<h2>Headers</h2>
+		</div>
+		<p class="lead-copy">Headers show content hierarchy.</p>
+		<p>The second-level heading, or module title, is used for naming the big subsections within a page. The word 'Headers' above this block of text is an example.</p>
+		<h3>Heading level three (h3)</h3>
+		<p>Following the page’s information hierarchy, third level headings should be used within the subsections. Avoid skipping levels – if your last header was an h2 and you want to label a block within that subsection, use an h3. If you need one within <em>that</em>, continue on with h4. HTML supports 6 levels of headers, but most content won't go deeper than 3 or 4.</p>
+		<h4>Heading level four</h4>
+		<p>Some pages probably won’t get to this level of hierarchy, but others might, especially in areas like sidebars. Here's some more filler text.</p>
+		<p>Physicians for Reproductive Health believes that access to contraception is of vital importance to women’s health because it allows them to determine the timing and spacing of pregnancies. In the medical world, we have studied birth control methods and their effects for decades.</p>
+	</div>
+</section>
+
+<section class="module">
+	<div class="content">
+		<div class="module__title">
+			<h2>Buttons & links</h2>
+		</div>
+		<div class="cta-row">
+			<a href="#" class="cta">Call To Action</a>
+			<a href="#" class="cta cta--red">Call to Action</a>
+		</div>
+		<p><a href="#">A normal inline link looks like this.</a></p>
   </div>
 </section>
 
 <section class="module">
-  <div class="content">
-    <div class="module__title">
-      <h2>Copy</h2>
-    </div>
-    <p class="lead-copy">This paragraph text is larger than normal. It’s generally used at the beginning of a page or section, to introduce the content. Blocks of text in this style shouldn’t be too long.</p>
-    <p>This is normal paragraph text. These styles apply to most of the body copy throughout the site. They support styles like <strong>bold</strong> and <em>italic</em> and can contain <a href="#">links</a> to other pages.</p>
-
-    <p>The level-one heading generally just happens once on a page, up in the hero or header area. It’s usually the page’s title. On this page, it's "Style Guide". And here's some filler copy from the PRH site, to show what a larger block of text looks like.</p>
-
-    <p>Physicians for Reproductive Health believes that access to contraception is of vital importance to women’s health because it allows them to determine the timing and spacing of pregnancies. In the medical world, we have studied birth control methods and their effects for decades. We know, based on enormous amounts of scientific evidence, that contraception is crucial to women’s well-being, their children’s health, and their ability to educate themselves, achieve career goals, care for and support their families, and otherwise participate in society. As physicians, we see every day how it improves our patients’ lives.</p>
-  </div>
-</section>
-<section class="module">
-  <div class="content">
-    <div class="module__title">
-      <h2>Headers</h2>
-    </div>
-    <p class="lead-copy">Headers show content hierarchy.</p>
-    <p>The second-level heading, or module title, is used for naming the big subsections within a page. The word 'Headers' above this block of text is an example.</p>
-    <h3>Heading level three (h3)</h3>
-    <p>Following the page’s information hierarchy, third level headings should be used within the subsections. Avoid skipping levels – if your last header was an h2 and you want to label a block within that subsection, use an h3. If you need one within <em>that</em>, continue on with h4. HTML supports 6 levels of headers, but most content won't go deeper than 3 or 4.</p>
-    <h4>Heading level four</h4>
-    <p>Some pages probably won’t get to this level of hierarchy, but others might, especially in areas like sidebars. Here's some more filler text.</p>
-    <p>Physicians for Reproductive Health believes that access to contraception is of vital importance to women’s health because it allows them to determine the timing and spacing of pregnancies. In the medical world, we have studied birth control methods and their effects for decades.</p>
-  </div>
-</section>
-<section class="module">
-  <div class="content">
-    <div class="module__title">
-      <h2>Buttons & links</h2>
-    </div>
-    <div class="cta-row">
-      <a href="#" class="cta">Call To Action</a>
-      <a href="#" class="cta cta--red">Call to Action</a>
-    </div>
-    <p><a href="#">A normal inline link looks like this.</a></p>
-  </div>
-</section>
-<section class="module">
-  <div class="content">
-    <div class="module__title">
-      <h2>Blockquotes</h2>
-    </div>
-    <blockquote>
-      <p>This is what the default styling for a blockquote looks like, including a citation with a secondary detail.</p>
-      <footer><cite>—Joel Hodgson, <span class="cite-origin">Minnesota</span></cite></footer>
-    </blockquote>
-  </div>
-</section>
-<section class="module">
-  <div class="content">
-    <div class="module__title">
-      <h2>Misc text elements</h2>
-    </div>
-    <p><span class="eyebrow">Eyebrow text | Also used for dates</span></p>
-  </div>
+	<div class="content">
+		<div class="module__title">
+			<h2>Blockquotes</h2>
+		</div>
+		<blockquote>
+			<p>This is what the default styling for a blockquote looks like, including a citation with a secondary detail.</p>
+			<footer><cite>—Joel Hodgson, <span class="cite-origin">Minnesota</span></cite></footer>
+		</blockquote>
+	</div>
 </section>
 
 <section class="module">
-  <div class="content">
-    <div class="module__title">
-      <h2>Test your markup</h2>
-    </div>
-    <p class="lead-copy">Below this, the page pulls from the wordpress admin area.</p>
-    <p>You can edit the page called 'Styleguide' and the contents will show up here. Any modules added that way will show below that.</p>
+	<div class="content">
+		<div class="module__title">
+			<h2>Misc text elements</h2>
+		</div>
+		<p><span class="eyebrow">Eyebrow text | Also used for dates</span></p>
+	</div>
+</section>
+
+<section class="module">
+	<div class="content">
+		<div class="module__title">
+			<h2>Test your markup</h2>
+		</div>
+		<p class="lead-copy">Below this, the page pulls from the wordpress admin area.</p>
+		<p>You can edit the page called 'Styleguide' and the contents will show up here. Any modules added that way will show below that.</p>
   </div>
 </section>
 
 <?php
 while ( have_posts() ) : the_post(); ?>
 <section class="module">
-  <div class="content">
-    <div class="module__title">
-      <h2><?php the_title(); ?></h2>
-    </div>
-    <?php the_content(); ?>
-  </div>
+	<div class="content">
+		<div class="module__title">
+			<h2><?php the_title(); ?></h2>
+		</div>
+		<?php the_content(); ?>
+	</div>
 </section>
 
 <?php
-  $groups = acf_get_field_groups();
-  $modules = array();
-  foreach( $groups as $group_key => $group ) {
-    $module = acf_get_fields($group);
-    $key = $group['title'];
-    $modules[$key] = array('module_name' => $group['title']);
-    foreach($module as $field_name => $field ) {
-      $f = $field['name'];
-      $modules[$key][$f] = get_field($f);
-    }
-  }
-  $page = new PageModules($modules);
-  $page->render(); 
+	$groups = acf_get_field_groups( get_the_ID() );
+	$modules = array();
+	foreach( $groups as $group_key => $group ) {
+		$module = acf_get_fields($group);
+		$key = $group['title'];
+		$modules[$key] = array('module_name' => $group['title']);
+		foreach($module as $field_name => $field ) {
+			$f = $field['name'];
+			$modules[$key][$f] = get_field($f);
+		}
+	}
+
+	$page = new PageModules($modules);
+	$page->render();
 endwhile; ?>
 
 </div> <!-- /page-container -->
