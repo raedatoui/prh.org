0a8be75743e2aa5377dc5cadc92d2962214aca38
Jack Gold <jgold@H9048.ad.hugeinc.com>
2017-04-13 16:37:25 -0400

    PRH-126 - getting most of the styles in place for the overview module

--- issue.php
+++ issue.php
@@ -4,6 +4,13 @@
  */
 
 get_header(); ?>
+
+<!-- placeholder hero so the content doesn't hit the nav -->
+<section class="hero module">
+  <div class="content">
+  </div>
+</section>
+
   <?php
     $page = new PageModules( get_the_ID() );
     $page->render();
--- template-parts/modules/overview.php
+++ template-parts/modules/overview.php
@@ -1,27 +1,49 @@
 <?php $module = $this->modules[OVERVIEW_MODULE['name']]; ?>
 <section class="overview module">
-  <div class="content">
-    <?php include( locate_template( 'template-parts/components/module-title.php', false, false ) ); ?>
+	<div class="content">
+		<?php include( locate_template( 'template-parts/components/module-title.php', false, false ) ); ?>
 
-    <div class="overview-text">
-    <?php
-      $full_content = $module[OVERVIEW_MODULE['content']]; 
-      $split_content = explode('<p><!--more--></p>', $full_content);
-      ?>
+		<div class="row top-xs">
+			<!-- Text component -->
+			<section class="overview-text col-md-8">
+				<?php
+				$full_content = $module[OVERVIEW_MODULE['content']]; 
+				$split_content = explode('<p><!--more--></p>', $full_content);
+				?>
 
-      <?php echo $split_content[0]; ?>
-      
-      <?php if (count($split_content) >= 1): ?>
-<!--         <a href="#collapsible-1" class="collapse-link">Read more</a>
+				<?php //echo $split_content[0]; ?>
+				<?php echo $full_content; ?>
+				
+				<?php if (count($split_content) >= 1): ?>
 
-        <div id="collapsible-1" class="collapsible overview-collapsible is-collapsed">
-          <div class="collapsible-content">
-            <?php //echo $split_content[1]; ?>
-          </div>
-        </div> -->
-      <?php endif; ?>
+					<!-- Here's basic logic for the "Read more" behavior, when that's up. -->
+					<!--<a href="#collapsible-1" class="collapse-link">Read more</a>
+
+					<div id="collapsible-1" class="collapsible overview-collapsible is-collapsed">
+						<div class="collapsible-content">
+							<?php //echo $split_content[1]; ?>
+						</div>
+					</div> -->
+				<?php endif; ?>
+			</section>
 
 
-    </div>
-  </div>
+			<aside class="sidebar resources-sidebar col-md-3 col-md-offset-1">
+				<h2 class="sidebar-header resources-header">
+					<?php echo $module[OVERVIEW_MODULE['resources_title']]; ?>
+				</h2>
+
+				<ul class="sidebar-links resources-links">
+					<?php foreach ( $module[OVERVIEW_MODULE['resources_links']] as $index => $link ): ?>
+						<li>
+							<a href="<?php echo $link[OVERVIEW_MODULE['resources_link_url']]; ?>" class="resource-link">
+								<?php echo $link[OVERVIEW_MODULE['resources_link_text']]; ?>
+							</a>
+						</li>
+					<?php endforeach; ?>
+				</ul>
+
+			</aside>
+		</div>
+	</div>
 </section>
