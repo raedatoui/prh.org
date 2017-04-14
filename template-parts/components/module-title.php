cf90020d8b667e342e0274eb08884d151298ef78
Jack Gold <jgold@H9048.ad.hugeinc.com>
2017-04-14 10:47:00 -0400

    updating module-title now that it's centered instead of left aligned

--- template-parts/components/module-title.php
+++ template-parts/components/module-title.php
@@ -1,5 +1,7 @@
 <?php if ( $module['config'][MODULE_OPTIONS['title']] != '' ): ?>
-	<div class="module__title">
-		<h2><?php echo $module['config'][MODULE_OPTIONS['title']] ?></h2>
-	</div>
+	<header class="row center-xs">
+		<div class="module__title">
+			<h2><?php echo $module['config'][MODULE_OPTIONS['title']] ?></h2>
+		</div>
+	</header>
 <?php endif; ?>
