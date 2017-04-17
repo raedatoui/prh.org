9c95a5d1f5c2528eff22a94bdd59ca577e5f7fa3
Jack Gold <jgold@H9048.ad.hugeinc.com>
2017-04-17 18:06:00 -0400

    making space for the sidebar to display tags, links, etc

--- single.php
+++ single.php
@@ -9,10 +9,8 @@ get_header(); ?>
 <section class="hero module">
 </section>
 
-<main class="main-content post-content module">
-	<div class="content">
-
-	<header>
+<main class="content module row row-top">
+	<header class="col-md-12">
 		<h1><?php the_title(); ?></h1>
 
 		<footer class="post-meta eyebrow">
@@ -26,18 +24,44 @@ get_header(); ?>
 				echo '<p class="post-intro">' . $intro . '</p>';
 			} ?>
 	</header>
+	<article class="main-content post-content col-md-8">
+			
+			<div class="post-body">
+				<?php the_content(); ?>
+			</div>
 
-		<hr>
-		<article class="post-body">
-			<?php the_content(); ?>
-		</article>
+			<hr>
 
-		<hr>
+			<footer class="post-byline">
+				<?php the_field('post_byline'); ?>
+			</footer>
+	</article>
 
-		<footer class="post-byline">
-			<?php the_field('post_byline'); ?>
-		</footer>
+	<div class="sidebar post-sidebar col-md-3 col-md-offset-1">
+
+		<?php 
+		$tags = get_the_tags($post->ID);
+		if ($tags):  ?>
+
+			<aside class="sidebar-block tags-block">
+				<div class="sidebar-content">
+					<h2 class="sidebar-header">Tagged</h2>
+					 <ul class="tags-list"> 
+		      <?php foreach($tags as $tag):  ?>
+						<li>
+			        <a class="tag"
+			            href="<?php bloginfo('url');?>/tag/<?php print_r($tag->slug);?>">
+			                  <?php print_r($tag->name); ?>
+			         </a>   
+			      </li>
+		      <?php endforeach; ?>
+					</ul>
+				</div>
+			</aside>
+
+		<?php endif; ?>
 	</div>
+
 </main>
 
 
