a51d18430669cdbc98437d8a49d63fbb942f8193
Jack Gold <jgold@H9048.ad.hugeinc.com>
2017-04-20 12:21:42 -0400

    one stylesheet request is...probably better than two, but i want to test this later

--- functions.php
+++ functions.php
@@ -71,8 +71,7 @@ add_action('widgets_init', 'prh_wp_theme_widgets_init');
  */
 function prh_wp_theme_scripts() {
 	wp_enqueue_style( 'prh-wp-theme-style', get_template_directory_uri() . '/css/main.css' );
-	wp_enqueue_style( 'prh-wp-theme-fonts', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:700|Roboto:400,400i,700,700i|Roboto:100&text=–+');
-	wp_enqueue_style( 'prh-wp-theme-fonts2', 'https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i');
+	wp_enqueue_style( 'prh-wp-theme-fonts', 'https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i|Roboto+Condensed:700|Roboto:400,400i,700,700i,100&text=–+');
 
 
 	// TODO: check if this is needed
