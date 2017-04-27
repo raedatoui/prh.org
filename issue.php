172aca8d9ce737a8e8fd711c1b134ea274bdec34
Jack Gold <jgold@H9048.ad.hugeinc.com>
2017-04-27 16:24:42 -0400

    baseline markup & starting on styles

--- issue.php
+++ issue.php
@@ -12,8 +12,41 @@ get_header(); ?>
 </section>
 
 <?php
-
 	$page = new PageModules( get_the_ID() );
 	$page->render();
+?>
 
-	get_footer();
+
+<section class="routing module">
+  <div class="content">
+
+  <div class="row">
+    
+    <?php $count = 0;
+      while ($count < 3): ?>
+    <div class="col-xs-12 col-md-4">
+      <div class="routing-block">
+
+        <h3 class="routing-title">Title</h3>
+
+        <div class="routing-content">
+          <p>Content content content. Blah blah blah.</p>
+        </div>
+
+        <div class="routing-ctas">
+          <a href="#">Link to something</a>
+          <a href="#">Another link to something</a>
+        </div>
+
+      </div>
+    </div>
+    <?php $count++; endwhile; ?>
+
+
+  </div>
+
+  </div>
+</section>
+
+
+<?php	get_footer(); ?>
