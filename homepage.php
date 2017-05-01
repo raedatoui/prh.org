7a93b1696b2b420afa93793f289cdb7ea945fd4d
Jack Gold <jgold@H9048.ad.hugeinc.com>
2017-05-01 14:06:30 -0400

    PRH-134 - admin refactor for the routing module + some style updates

--- homepage.php
+++ homepage.php
@@ -15,42 +15,5 @@ get_header();?>
 		$page->render();
 	endwhile; // End of the loop. ?>
 
-  <section class="routing module">
-  <div class="content">
-
-  <header class="row center-xs">
-    <div class="module__title">
-      <h2>Our Focus</h2>
-    </div>
-  </header>
-
-  <div class="row routing-row">
-    
-    <?php $count = 0;
-      while ($count < 3): ?>
-    <div class="col-xs-12 col-md-4">
-      <div class="routing-block">
-
-        <div class="routing-content">
-        <h3 class="routing-title">Title</h3>
-          <p>Content content content. Blah blah blah.</p>
-        </div>
-
-        <div class="routing-ctas">
-          <a class="cta--link" href="#">Link to something</a>
-          <a class="cta--link" href="#">Another link to something</a>
-        </div>
-
-      </div>
-    </div>
-    <?php $count++; endwhile; ?>
-
-
-  </div>
-
-  </div>
-</section>
-
-
 <?php
 get_footer();
