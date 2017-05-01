d13a203d1ecf696a089cb6f325013cc17cd39538
Jack Gold <jgold@H9048.ad.hugeinc.com>
2017-05-01 11:55:40 -0400

    moving routing module to homepage (still not admin-ready, just markup)

--- issue.php
+++ issue.php
@@ -16,37 +16,4 @@ get_header(); ?>
 	$page->render();
 ?>
 
-
-<section class="routing module">
-  <div class="content">
-
-  <div class="row">
-    
-    <?php $count = 0;
-      while ($count < 3): ?>
-    <div class="col-xs-12 col-md-4">
-      <div class="routing-block">
-
-        <h3 class="routing-title">Title</h3>
-
-        <div class="routing-content">
-          <p>Content content content. Blah blah blah.</p>
-        </div>
-
-        <div class="routing-ctas">
-          <a href="#">Link to something</a>
-          <a href="#">Another link to something</a>
-        </div>
-
-      </div>
-    </div>
-    <?php $count++; endwhile; ?>
-
-
-  </div>
-
-  </div>
-</section>
-
-
 <?php	get_footer(); ?>
