b1a69007272111526efea393c8c58e7d55aeb2b6
Jack Gold <jgold@H9048.ad.hugeinc.com>
2017-05-02 17:57:03 -0400

    PRH-135 - main nav is pretty much done. plus a few style updates.

--- header.php
+++ header.php
@@ -64,15 +64,4 @@
 				</nav>
 				</div>
 			</header>
-			<!-- <header role="banner" class="site-header" id="site-header">
-				<nav role="navigation" class="site-nav row middle-xs">
-						<a href="/" class="col-sm-3 col-md-3">
-							<img class="logo" alt="" src="<?php //echo_theme_uri(); ?>/images/optimized/prh_nav_logo.svg" />
-						</a>
-						<?php// wp_nav_menu(); ?>
-						<div class="col-xs-12 col-sm-1 col-md-2" style="text-align: right;">Donate</div>
-					<?php //get_search_form(); ?>
-				</nav>
-
-			</header>
 			<div class="nav-mask" id="nav-mask"></div> -->
