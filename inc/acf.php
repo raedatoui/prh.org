fc10f40e004ae29ca010abfdd8bc71baa837cce2
HUGE | Raed Atoui <ratoui@hugeinc.com>
2017-05-04 16:42:25 -0400

    wip media contact

--- inc/acf.php
+++ inc/acf.php
@@ -22,3 +22,42 @@ function prh_acf_json_load_point( $paths ) {
 	// return
 	return $paths;
 }
+
+
+/**
+ * This function sync the media contact fields when the user changes the name.
+*/
+function prh_sync_media_contact_values()
+{
+	?>
+	<script type="text/javascript">
+	(function($){
+		$('*[data-name="media_contact_name"] select').change(function(){
+			var value = $(this).val();
+			$('*[data-name="media_contact_email"] select').val(value);
+			$('*[data-name="media_contact_phone"] select').val(value);
+		});
+	})(jQuery);
+	</script>
+	<?php
+}
+
+add_action('acf/input/admin_footer', 'prh_sync_media_contact_values');
+
+/**
+ * This function disables the email and phone fields of the Media Contact
+ * group allowing them to sync with the name changes.
+ */
+function prh_disable_media_contact_fields(){
+	?>
+	<script type="text/javascript">
+	(function($) {
+		$(document).ready(function(){
+			$('*[data-name="media_contact_email"] select').attr('disabled', true);
+			$('*[data-name="media_contact_phone"] select').attr('disabled', true);
+  		});
+	})(jQuery);
+	</script>
+	<?php
+}
+add_action('admin_print_footer_scripts', 'prh_disable_media_contact_fields');
