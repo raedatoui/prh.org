87d6d259ce15f23872e70e7d8cb624bef53c05d4
HUGE | Raed Atoui <ratoui@hugeinc.com>
2017-05-05 15:32:34 -0400

    fix for jump links

--- template-parts/modules/page-hero.php
+++ template-parts/modules/page-hero.php
@@ -1,13 +1,13 @@
 <section class="hero module">
 	<div class="content">
 
-		<h1 class="hero__header"><?php 
+		<h1 class="hero__header"><?php
 			// Strip auto-added paragraph tags since <h1><p> isn't valid, (but leave any other html intact)
 		  echo str_replace(array('<p>', '</p>'), '', $module_title);
 		?></h1>
 		<p class="hero__subhead">
 			<?php foreach ($module[PAGE_HERO_MODULE['jump_links']] as $item): ?>
-			<a class="hero__link underline jump" href="#<?php echo $item ?>"><?php echo  $item ?></a>
+			<a class="hero__link jump" href="#<?php echo $item ?>"><?php echo  $item ?></a>
 			<?php endforeach; ?>
 		</p>
 	</div>
