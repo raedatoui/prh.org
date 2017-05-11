be3e9fd84edb43f80a8685034690a8f1bd004498
Jack Gold <jgold@H9048.ad.hugeinc.com>
2017-05-11 16:32:58 -0400

    PRH-111 - adding pagination to search results page

--- search.php
+++ search.php
@@ -97,15 +97,32 @@ $cats = get_categories();
 						</aside>
 					</div>
 
+				<nav class="pagination results-pagination">
+					<?php 
+
+					global $wp_query;
+
+					$big = 999999999; // need an unlikely integer
+					$translated = __( 'Page ', 'mytextdomain' ); // Supply translatable string
+
+					echo paginate_links( array(
+						'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+						'format' => '?paged=%#%',
+						'current' => max( 1, get_query_var('paged') ),
+						'total' => $wp_query->max_num_pages,
+						'before_page_number' => '<span class="visually-hidden">'.$translated.' </span>',
+						'prev_text' => '',
+						'next_text' => ''
+					) ); ?>
+				</nav>
+
+
+
 				<?php 
-
-				the_posts_navigation();
-
+				// the_posts_navigation();
 				// else :
-
-				// 	get_template_part( 'template-parts/content', 'none' );
-
-				endif; ?>
+				// 	get_template_part( 'template-parts/content', 'none' ); ?>
+				<?php endif; ?>
 			</div>
 		</div>
 
