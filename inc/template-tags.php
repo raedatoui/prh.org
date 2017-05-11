caf5cd6bd5fffce0198382aa2b4dd5620409f4d2
Jack Gold <jgold@H9048.ad.hugeinc.com>
2017-05-11 15:58:09 -0400

    working on the search results page markup

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -25,16 +25,10 @@ function prh_wp_theme_posted_on() {
 	);
 
 	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'prh-wp-theme' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
-	);
+		esc_html_x( '%s', 'post date', 'prh-wp-theme' ),
+		 $time_string);
 
-	$byline = sprintf(
-		esc_html_x( 'by %s', 'post author', 'prh-wp-theme' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	);
-
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+	echo '<span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
 
 }
 endif;
--- search.php
+++ search.php
@@ -7,41 +7,109 @@
  * @package prh-wp-theme
  */
 
-get_header(); ?>
+get_header(); 
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+// Set up the query of all our content types (to make the list of filters)
+$ct_args = array('public' => true);
+$ct_output = 'objects';
+$ct_operator = 'and';
+$content_types = get_post_types($ct_args, $ct_output, $ct_operator);
 
-		<?php
-		if ( have_posts() ) : ?>
+// Same, but for categories
+$cats = get_categories();
+?>
 
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'prh-wp-theme' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
+	<!-- Persistent search bar above the results -->
+	<div class="search-open hero search-hero">
+		<div id="search-bar" class="search-bar">
+			<div class="content search-bar-content">
+			<?php get_search_form(); ?>
+			</div>
+		</div>
+	</div>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+	<main id="main" class="site-main search-main" role="main">
+		<div class="module search-results-module">
+			<div class="content">
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
+				<?php
+				if ( have_posts() ) : 
+				global $wp_query;
+				$total_results = $wp_query->found_posts;
+				?>
 
-			endwhile;
+				<header class="page-header row">
+					<h2 class="page-title search-page-title col-xs">
+						<?php printf( esc_html__( '%s results for &ldquo;%s&rdquo;', 'prh-wp-theme' ), $total_results, '<span class="search-query-term">' . get_search_query() . '</span>' ); ?>
+					</h2>
+				</header><!-- .page-header -->
 
-			the_posts_navigation();
+					<!-- Left side (results area) -->
+				<div class="row">
+					<div class="col-xs-12 col-md-9 col-lg-8 search-results">
+						<?php
+						// the markup for an individual result is in template-parts/content-search.php
+						while ( have_posts() ) : the_post();
+							get_template_part( 'template-parts/content', 'search' );
+						endwhile; ?>
+					</div>
 
-		else :
+					<!-- Right side (filtering) -->
+					<div class="sidebar post-sidebar col-xs-12 col-md-3 col-lg-offset-1 search-filters">
 
-			get_template_part( 'template-parts/content', 'none' );
+						<aside class="sidebar-block media-contact-block">
+							<div class="sidebar-content">
+								<h2 class="sidebar-header">Content type</h2>
 
-		endif; ?>
+								<ul class="filter-list checkbox-list type-list">
+								<?php
+								foreach ( $content_types as $type ): ?>
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
+									<li class="checkbox-item">
+										<label>
+											<input type="checkbox" class="filter filter-type" id="filter-type-<?php echo $type->slug; ?>">
+											<?php echo $type->labels->singular_name; ?>
+										</label>
+									</li>
+								<?php endforeach; ?>
+								</ul>
+
+							</div>
+						</aside>
+
+						<aside class="sidebar-block media-contact-block">
+							<div class="sidebar-content">
+								<h2 class="sidebar-header">Category</h2>
+
+								<ul class="filter-list checkbox-list type-list">
+								<?php foreach ( $cats as $cat ): ?>
+
+									<li class="checkbox-item">
+										<label>
+											<input type="checkbox" class="filter filter-cat" id="filter-cat-<?php echo $cat->slug; ?>">
+											<?php echo $cat->name ?>
+										</label>
+									</li>
+
+								<?php endforeach; ?>
+								</ul>
+							</div>
+						</aside>
+					</div>
+
+				<?php 
+
+				the_posts_navigation();
+
+				// else :
+
+				// 	get_template_part( 'template-parts/content', 'none' );
+
+				endif; ?>
+			</div>
+		</div>
+
+	</main><!-- #main -->
 
 <?php
 get_sidebar();
