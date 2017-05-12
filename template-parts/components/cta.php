fcec15ebe1304f594d91a0aad17ef9226241ee4f
HUGE | Raed Atoui <ratoui@hugeinc.com>
2017-05-12 17:12:59 -0400

    make donate red

--- template-parts/components/cta.php
+++ template-parts/components/cta.php
@@ -1,8 +1,15 @@
 <?php if ( $module['config'][MODULE_OPTIONS['use_cta']] ): ?>
+	<?php
+		$cta = $module['config'][MODULE_OPTIONS['cta']][0];
+		$cta_label = trim($cta[CTA_COMPONENT['label']]);
+		$cta_class = "cta";
+		if ( $cta_label == "Donate" ) {
+			$cta_class = "cta cta--red";
+		}
+	?>
 	<div class="row cta-row">
-		<?php $cta = $module['config'][MODULE_OPTIONS['cta']][0];  ?>
-		<a class="cta" href="<? echo $cta[CTA_COMPONENT['link']];?>">
-			<? echo $cta[CTA_COMPONENT['label']]; ?>
+		<a class="<?php echo $cta_class; ?>" href="<? echo $cta[CTA_COMPONENT['link']];?>">
+			<?php echo $cta_label; ?>
 		</a>
 	</div>
 <?php endif; ?>
