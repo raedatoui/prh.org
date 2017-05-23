e84ccf1ffc4428057e3f1a70bb7edab45815a9e0
HUGE | Raed Atoui <ratoui@hugeinc.com>
2017-05-23 15:30:42 -0400

    Wip

---
+++ template-parts/archive-events.php
@@ -0,0 +1,78 @@
+<?php
+	wp_reset_postdata();
+	$today = date('Ymd');
+	$upcoming_events = get_posts( array (
+		'numberposts'	=> -1,
+		'post_type'		=> 'prh_event',
+		'orderby'		=> 'meta_value',
+		'order'			=> 'DESC',
+		'meta_query' => array(
+			array(
+				'key'		=> 'event_date',
+				'compare'	=> '>=',
+				'value'		=> $today
+			)
+		)
+	) );
+	$past_events = array();
+	print_r($upcoming_events);
+
+?>
+
+<section class="hero module shiny-hero">
+	<div class="content">
+		<h1 class="hero__header">Attend an Event</h1>
+	</div>
+</section>
+
+<section class="routing module" id="our-focus">
+	<div class="content">
+		<?php if ( $upcoming_events->have_posts() ) : ?>
+			<div class="row">
+				<div class="col-xs-12">
+					<p class="focus-lead-copy">Upcoming Events</p>
+				</div>
+			</div>
+			<div class="row">
+				<?php while ( $upcoming_events->have_posts() ) : $upcoming_eventsthe_post(); ?>
+					<h2><?php the_title(); ?></h2>
+				<?php endwhile; ?>
+			</div>
+		<?php else: ?>
+		<div class="row">
+			<div class="col-xs-12">
+				<p class="focus-lead-copy">No Upcoming Events</p>
+			</div>
+		</div>
+		<?php endif; ?>
+  </div>
+</section>
+
+<section class="routing module" id="our-focus">
+	<div class="content">
+		<div class="row">
+			<div class="col-xs-12">
+				<p class="focus-lead-copy">Past Events</p>
+			</div>
+		</div>
+  </div>
+</section>
+
+<?php
+	$donate_bg = get_template_directory_uri() . '/images/optimized/donate-module.jpg';
+	$front_id = get_option(page_on_front);
+	$headline = get_field(DONATE_MODULE['headline'], $front_id);
+	$text = get_field(DONATE_MODULE['text'], $front_id);
+	$cta_url = get_field(DONATE_MODULE['cta_url'], $front_id);
+	$cta_text = get_field(DONATE_MODULE['cta_text'], $front_id);
+?>
+
+<section class="module donate-module" style="background-image: url(<?php echo $donate_bg; ?>);">
+	<div class="center-xs donate-content">
+		<h2 class="donate-header"><?php echo $headline; ?></h2>
+		<p class="donate-copy"><?php echo $text; ?></p>
+		<div class="cta-row center-xs donate-cta">
+		   <a class="cta cta--red" href="<?php echo $cta_url; ?>"><?php echo $cta_text; ?></a>
+		</div>
+	</div>
+</section>
