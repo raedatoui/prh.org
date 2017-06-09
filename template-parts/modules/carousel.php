dbce35e433c551026eab9885748e1b5076c57f11
HUGE | Raed Atoui <ratoui@hugeinc.com>
2017-06-09 17:02:17 -0400

    inactive slides when no links are used

--- template-parts/modules/carousel.php
+++ template-parts/modules/carousel.php
@@ -6,8 +6,17 @@
 			<?php foreach ( $module[CAROUSEL_MODULE['slides']] as $index => $slide ): ?>
 
 				<div class="slide slide-<?php echo $index ?>">
-					<?php $url = "http://" . $slide[CAROUSEL_MODULE['link']]; ?>
-					<a class="slide-link" href="<?php echo $url ?>" target="<?php echo get_url_target( $url ); ?>">
+					<?php
+						$link_class = "slide-link";
+						$url = "http://" . $slide[CAROUSEL_MODULE['link']];
+						$link_target =  get_url_target( $url );
+						if (  $slide[CAROUSEL_MODULE['link']] == "" ) {
+							$link_class =  "slide-link inactive-slide";
+							$url = "#";
+							$link_target = "_self";
+						}
+					?>
+					<a class="<?php echo $link_class; ?>" href="<?php echo $url ?>" target="<?php echo $link_target; ?>">
 						<div class="slide-half">
 							<img src="<?php echo $slide[CAROUSEL_MODULE['image']]['url'] ?>"
 							alt=""/>
@@ -23,6 +32,7 @@
 							?>
 						</div>
 					</a>
+
 				</div>
 
 			<?php endforeach; ?>
