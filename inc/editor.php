4a4eaf4a0831c4f4ae797125096d065c5ed564c4
Raed Atoui <reed@otoole.net>
2018-07-29 09:43:28 -0400

    VOC allow editor to use tables

--- inc/editor.php
+++ inc/editor.php
@@ -144,3 +144,16 @@ function prh_admin_menu() {
 }
 
 add_action('admin_menu', 'prh_admin_menu');
+
+
+function add_the_table_button( $buttons ) {
+    array_push( $buttons, 'separator', 'table' );
+    return $buttons;
+}
+add_filter( 'mce_buttons', 'add_the_table_button' );
+
+function add_the_table_plugin( $plugins ) {
+      $plugins['table'] = content_url() . '/tinymce-plugins/table/plugin.min.js';
+      return $plugins;
+}
+add_filter( 'mce_external_plugins', 'add_the_table_plugin' );
