a590bd5769ebbcfd2a22df8903c1e8b9d8ed76a8
Raed Atoui <reed@otoole.net>
2018-08-01 00:48:18 -0400

    VOC form styles

--- template-parts/modules/voc-form.php
+++ template-parts/modules/voc-form.php
@@ -7,21 +7,42 @@
 			</div>
 			<div class="col col2">
 				<div class="input">
-					<input type="text" name="cptTitle" id="cptTitle" placeholder="First name" />
+					<input type="text" name="first" id="first" placeholder="First name" />
 				</div>
 				<div class="input">
-					<input type="text" name="cptTitle" id="cptTitle" placeholder="email"/>
+					<input type="text" name="email" id="email" placeholder="email"/>
 				</div>
 				<div class="input">
-					<input type="text" name="cptTitle" id="cptTitle" placeholder="last name"/>
+					<input type="text" name="last" id="last" placeholder="last name"/>
 				</div>
 				<div class="input">
-					<input type="text" name="cptTitle" id="cptTitle" placeholder="photo upload"/>
+					<input class="inputfile" type="file" name="photo" id="photo"/>
+					<label for="photo">
+						<span>Photo upload</span>
+						<div class="icon"><span class="visually-hidden">upload</span></div>
+					</label>
 				</div>
 				<div class="input">
 					<input type="hidden" name="post_type" id="post_type" value="phys_story" />
 				</div>
-				<button type="submit"><?php _e('Submit', 'mytextdomain') ?></button>
+
+				<button type="submit">
+					<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 100% 100%">
+						<defs>
+							<style>.cls-1{fill:url(#linear-gradient);}</style>
+							<linearGradient id="linear-gradient" y1="50%" x2="100%" y2="50%" gradientUnits="userSpaceOnUse">
+								<stop offset="0" stop-color="#adbdff"/>
+								<stop offset="1" stop-color="#0580c3"/>
+							</linearGradient>
+						</defs>
+						<g id="Layer_2" data-name="Layer 2">
+							<g id="Layer_1-2" data-name="Layer 1">
+								<rect class="cls-1" width="100%" height="100%"/>
+							</g>
+						</g>
+					</svg>
+					<span>SUBMIT</span>
+				</button>
 			</div>
 			<?php wp_nonce_field( 'cpt_nonce_action', 'cpt_nonce_field' ); ?>
 		</form>	
