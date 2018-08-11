ac08be33fc4e2c3ce1e9cd94dc28b2d1b09c5611
Raed Atoui <raed.atoui@gmail.com>
2018-08-11 18:26:39 -0400

    working story submission

--- template-parts/modules/voc-form.php
+++ template-parts/modules/voc-form.php
@@ -1,30 +1,85 @@
 <section class="module voc">
 	<div class="content voc-form">
-        <?php include( locate_template( 'template-parts/components/module-title.php', false, false ) ); ?>
-		<form method="post">
+		<?php include( locate_template( 'template-parts/components/module-title.php', false, false ) ); ?>
+		<form method="post" action="" name="new_story">
 			<div class="col col1">
-				<textarea name="cptContent" id="cptContent" placeholder="My story is..."></textarea>
+				<textarea name="storyContent" id="story-content" placeholder="My story is..."></textarea>
 			</div>
 			<div class="col col2">
 				<div class="input">
-					<input type="text" name="first" id="first" placeholder="First name" />
+					<input type="text" name="storyFirstName" id="story-first-name" placeholder="First name" />
 				</div>
 				<div class="input">
-					<input type="text" name="email" id="email" placeholder="email"/>
+					<input type="text" name="storyEmail" id="story-email" placeholder="Email"/>
 				</div>
 				<div class="input">
-					<input type="text" name="last" id="last" placeholder="last name"/>
+					<input type="text" name="storyLastName" id="story-last" placeholder="Last name"/>
 				</div>
 				<div class="input">
-					<input class="inputfile" type="file" name="photo" id="photo"/>
+					<select name="storyState" id="story-state" placeholder="State">
+						<option value="AL">Alabama</option>
+						<option value="AK">Alaska</option>
+						<option value="AZ">Arizona</option>
+						<option value="AR">Arkansas</option>
+						<option value="CA">California</option>
+						<option value="CO">Colorado</option>
+						<option value="CT">Connecticut</option>
+						<option value="DE">Delaware</option>
+						<option value="DC">District Of Columbia</option>
+						<option value="FL">Florida</option>
+						<option value="GA">Georgia</option>
+						<option value="HI">Hawaii</option>
+						<option value="ID">Idaho</option>
+						<option value="IL">Illinois</option>
+						<option value="IN">Indiana</option>
+						<option value="IA">Iowa</option>
+						<option value="KS">Kansas</option>
+						<option value="KY">Kentucky</option>
+						<option value="LA">Louisiana</option>
+						<option value="ME">Maine</option>
+						<option value="MD">Maryland</option>
+						<option value="MA">Massachusetts</option>
+						<option value="MI">Michigan</option>
+						<option value="MN">Minnesota</option>
+						<option value="MS">Mississippi</option>
+						<option value="MO">Missouri</option>
+						<option value="MT">Montana</option>
+						<option value="NE">Nebraska</option>
+						<option value="NV">Nevada</option>
+						<option value="NH">New Hampshire</option>
+						<option value="NJ">New Jersey</option>
+						<option value="NM">New Mexico</option>
+						<option value="NY">New York</option>
+						<option value="NC">North Carolina</option>
+						<option value="ND">North Dakota</option>
+						<option value="OH">Ohio</option>
+						<option value="OK">Oklahoma</option>
+						<option value="OR">Oregon</option>
+						<option value="PA">Pennsylvania</option>
+						<option value="RI">Rhode Island</option>
+						<option value="SC">South Carolina</option>
+						<option value="SD">South Dakota</option>
+						<option value="TN">Tennessee</option>
+						<option value="TX">Texas</option>
+						<option value="UT">Utah</option>
+						<option value="VT">Vermont</option>
+						<option value="VA">Virginia</option>
+						<option value="WA">Washington</option>
+						<option value="WV">West Virginia</option>
+						<option value="WI">Wisconsin</option>
+						<option value="WY">Wyoming</option>
+					</select>
+				</div>
+				<div class="input">
+					<input class="inputfile" type="file" name="storyPhoto" id="story-photo"/>
 					<label for="photo">
 						<span>Photo upload</span>
 						<div class="icon"><span class="visually-hidden">upload</span></div>
 					</label>
 				</div>
-				<div class="input">
+				<!-- <div class="input">
 					<input type="hidden" name="post_type" id="post_type" value="phys_story" />
-				</div>
+				</div> -->
 
 				<button type="submit">
 					<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -47,4 +102,30 @@
 			<?php wp_nonce_field( 'cpt_nonce_action', 'cpt_nonce_field' ); ?>
 		</form>	
 	</div>
+	<?php
+		if ($_SERVER['REQUEST_METHOD'] == 'POST' &&  isset( $_POST['cpt_nonce_field'] ) && wp_verify_nonce( $_POST['cpt_nonce_field'], 'cpt_nonce_action' ) ) {
+			
+			// create post object with the form values
+			$my_cptpost_args = array(
+				'post_title' => ($_POST['storyFirstName'] . " ". $_POST['storyLastName']),
+				'post_content' => $_POST['storyContent'],
+				'post_status' => 'pending',
+				'post_type' => 'phys_story',
+			);
+			// insert the post into the database
+			$cpt_id = wp_insert_post( $my_cptpost_args, $wp_error);
+			update_field('voc_email', $_POST['storyEmail'], $cpt_id);
+			update_field('voc_first_name', $_POST['storyFirstName'], $cpt_id);
+			update_field('voc_last_name', $_POST['storyLastName'], $cpt_id);
+			update_field('voc_state', $_POST['storyState'], $cpt_id);
+			if ($_FILES) {
+				foreach ($_FILES as $file => $array) {
+					$newupload = insert_attachment($file,$post_id);
+					$attchmentIds[] = $newupload;
+				}
+			};	
+		}
+	?>
+	
 </section>
+
